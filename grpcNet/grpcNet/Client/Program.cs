32f26a92e9f2e6adcc41b81e31746aa3b0fff656
JoshDiaz <josethp1@hotmail.com>
2026-08-14 22:30:11 -0500

    Inicio con gRCP

---
+++ grpcNet/grpcNet/Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Vaxi;
+
+const string serverPoint = "127.0.0.1:5000";
+
+Grpc.Core.Channel canal = new Grpc.Core.Channel(serverPoint, Grpc.Core.ChannelCredentials.Insecure);
+
+canal.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == System.Threading.Tasks.TaskStatus.RanToCompletion)
+    {
+        Console.WriteLine("El cliente se conecto al servidor GRPC correctamente");
+    }
+});
+
+var client = new Operaciones.OperacionesClient(canal);
+
+canal.ShutdownAsync().Wait();
+Console.ReadKey();
---
+++ grpcNet/grpcNet/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿const int Port = 5000;
+Grpc.Core.Server server = null;
+try 
+{
+    server = new Grpc.Core.Server()
+    {
+        Ports = { new Grpc.Core.ServerPort("localhost", Port, Grpc.Core.ServerCredentials.Insecure)}
+    };
+    server.Start();
+    Console.WriteLine("El servidor se esta ejecutando en el puerto :" + Port);
+    Console.ReadKey();
+}
+catch (IOException e)
+{
+    Console.WriteLine("Errores en el servidor" + e.Message);
+}
+finally
+{
+    if(server != null)
+        server.ShutdownAsync().Wait();
+}
